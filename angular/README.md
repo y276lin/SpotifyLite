e5f47250bc0e946bdb4f3492ff2e2c2f03f561c2
y276lin <y276lin@edu.uwaterloo.ca>
2018-09-16 12:11:52 -0700

    enforce https

--- angular/README.md
+++
@@ -1,7 +0,0 @@
-# spotifyLite - Angular 2 + NodeJS
-
-- [Demo] -> https://spotifylite.herokuapp.com/
-- Search music by Artist and preview without logging in
-- For full music, click on direct link to view on Spotify
-- Full access to 30 million+ songs on Spotify
-- Server repo -> https://github.com/x65han/SpotifyLite-Server
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './components/navbar.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-
+  constructor() {
+      // enforce HTTPS
+      if (!window.location.href.includes('localhost') && !window.location.href.includes('https')) {
+        window.location.href = "https://spotifylite.herokuapp.com" + window.location.pathname;
+      }
+  }
 }
